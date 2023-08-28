424cbbd9327503152501927b4b0692d84b6e5a1a
Usman Zia <u_zia@uncg.edu>
2023-08-28 11:12:21 -0400

    creating app

---
+++ GLookUp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { QuestionnaireComponent } from './components/questionnaire/questionnaire.component';
+import { BackButtonComponent } from './components/back-button/back-button.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    QuestionnaireComponent,
+    BackButtonComponent,
+    ProfileComponent,
+    CustomButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ GLookUp/src/app/components/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent {
+  onSubmit() {
+    console.log('submited!')
+  }
+}
