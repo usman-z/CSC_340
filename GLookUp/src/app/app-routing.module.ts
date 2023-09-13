e2677ec5c6597b31e52c4dc5785bab11d1f78053
Usman Zia <u_zia@uncg.edu>
2023-09-12 20:58:37 -0400

    Home page done

--- GLookUp/src/app/app-routing.module.ts
+++ GLookUp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [
