cf744a48812700a9e1a0eff3b972a701e15c8465
Usman Zia <u_zia@uncg.edu>
2023-11-02 08:54:08 -0400

    frontend and backend

---
+++ Backend/glookup/src/main/java/com/server/glookup/GlookupApplication.java
@@ -0,0 +1,13 @@
+package com.server.glookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlookupApplication.class, args);
+	}
+
+}
---
+++ Backend/glookup/src/test/java/com/server/glookup/GlookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.glookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
